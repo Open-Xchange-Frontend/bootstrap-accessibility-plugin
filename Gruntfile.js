eb60391f486296a657ec516c560b5e0c8b67ee61
Christoph Hellweg <christoph.hellweg@open-xchange.com>
2015-02-19 10:26:21 +0100

    Cleanup plugin code and rebuild dist files

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,6 @@ module.exports = function(grunt) {
 
 		license:
 			'/* ========================================================================' + '\n' +
-
 			'* Extends Bootstrap v3.1.1' + '\n' + '\n' +
 			'* Copyright (c) <2014> eBay Software Foundation' + '\n' +  '\n' +
 			'* All rights reserved.' + '\n' +   '\n' +
@@ -18,7 +17,7 @@ module.exports = function(grunt) {
 			'* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.' + '\n' +'\n' +
 			'* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.' + '\n' +'\n' +
 			'* Neither the name of eBay or any of its subsidiaries or affiliates nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.' + '\n' +'\n' +
-			'* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.' + '\n' +'\n' +
+			'* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.' + '\n' +
 			'* ======================================================================== */' + '\n',
 
 		jshint: {
@@ -43,7 +42,7 @@ module.exports = function(grunt) {
 
 	    concat: {
 	      options: {
-	        banner: '<%= license %> \n\n(function($) {\n\n  "use strict";\n',
+	        banner: '<%= license %>\n(function($) {\n\n    \'use strict\';\n\n',
 	        separator: '\n',
 	        stripBanners: false,
 			footer: '\n})(jQuery);'
--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,74 +1,73 @@
+    // DROPDOWN Extension
+    // ===============================
+    function a11yDropdown() {
+        var toggle = this,
+            root = toggle.parent(),
+            menu = root.find('ul'),
+            items = menu.find('li');
 
-  // DROPDOWN Extension
-  // ===============================
-  function a11yDropdown() {
-    var toggle = this,
-        root = toggle.parent(),
-        menu = root.find('ul'),
-        items = menu.find('li');
+        toggle
+            .attr({ 'aria-haspopup': true, 'aria-expanded': false });
+        menu
+            .not('[role]')
+            .attr({ role: 'menu' });
+        items
+            .filter(':not([role])')
+            .attr({ role: 'presentation' });
+        items
+            .find('a:not([role])')
+            .attr({ role: 'menuitem', tabIndex: '-1' });
 
-    toggle
-      .attr({ 'aria-haspopup': true, 'aria-expanded': false });
-    menu
-      .not('[role]')
-      .attr({ role: 'menu' });
-    items
-      .filter(':not([role])')
-      .attr({ role: 'presentation' });
-    items
-      .find('a:not([role])')
-      .attr({ role: 'menuitem', tabIndex: '-1' });
+        root.on({
+            'shown.bs.dropdown': function(e, args) {
+                toggle.attr({ 'aria-expanded': true });
+                var el = args.relatedTarget
+                if (!$(el).data('preventFocus')) {
+                    setTimeout(function() {
+                        $('a[role^="menuitem"]',items).first(':visible').focus();
+                    }, 200);
+                }
+                $(el).removeData('preventFocus')
+            },
+            'hidden.bs.dropdown': function(e) {
+                toggle.attr('aria-expanded','false');
+            }
+        });
 
-    root.on({
-      'shown.bs.dropdown': function(e, args) {
-        toggle.attr({ 'aria-expanded': true });
-        var el = args.relatedTarget
-        if (!$(el).data('preventFocus')) {
-          setTimeout(function() {
-            $('a[role^="menuitem"]',items).first(':visible').focus();
-          }, 200);
-        }
-        $(el).removeData('preventFocus')
-      },
-      'hidden.bs.dropdown': function(e) {
-        toggle.attr('aria-expanded','false');
-      }
-    });
-
-    return this;
-  }
-
-  $(document)
-    .on('focusout.dropdown.data-api', '.dropdown-menu', function(e) {
-      var that = this;
-      setTimeout(function() {
-        if(!$.contains(that, document.activeElement)) {
-          $(that)
-            .parent()
-            .removeClass('open')
-            .find('[data-toggle=dropdown]')
-            .attr({ 'aria-expanded': false });
-        }
-      }, 150);
-    })
-    .on('keydown.bs.dropdown.data-api', '[data-toggle=dropdown], [role=menu]' , function (e) {
-      //Adding Space Key Behaviour, opens on spacebar
-      if (e.which == 32 && $(e.target).is('a')) {
-        $(e.target).click();
-      }
-    })
-    .on('mousedown.bs.dropdown.data-api', '[data-toggle=dropdown]', function (e) {
-      $(this).data('preventFocus', true);
-    });
-
-  var dropdownConstructor = $.fn.dropdown.Constructor,
-      dropdownFn = $.fn.dropdown;
-
-  $.fn.dropdown = function (option) {
-    if (!$(this).data('bs.dropdown')) a11yDropdown.apply(this);
-    if (typeof option === 'string' && option === 'toggle') {
-      $(this).data('preventFocus', true);
+        return this;
     }
-    return dropdownFn.apply(this, arguments);
-  };
-  $.fn.dropdown.Constructor = dropdownConstructor;
+
+    $(document)
+        .on('focusout.dropdown.data-api', '.dropdown-menu', function(e) {
+            var that = this;
+            setTimeout(function() {
+                if (!$.contains(that, document.activeElement)) {
+                    $(that)
+                        .parent()
+                        .removeClass('open')
+                        .find('[data-toggle=dropdown]')
+                        .attr({ 'aria-expanded': false });
+                }
+            }, 150);
+        })
+        .on('keydown.bs.dropdown.data-api', '[data-toggle=dropdown], [role=menu]' , function (e) {
+            //Adding Space Key Behaviour, opens on spacebar
+            if (e.which == 32 && $(e.target).is('a')) {
+                $(e.target).click();
+            }
+        })
+        .on('mousedown.bs.dropdown.data-api', '[data-toggle=dropdown]', function (e) {
+            $(this).data('preventFocus', true);
+        });
+
+    var dropdownConstructor = $.fn.dropdown.Constructor,
+            dropdownFn = $.fn.dropdown;
+
+    $.fn.dropdown = function (option) {
+        if (!$(this).data('bs.dropdown')) a11yDropdown.apply(this);
+        if (typeof option === 'string' && option === 'toggle') {
+            $(this).data('preventFocus', true);
+        }
+        return dropdownFn.apply(this, arguments);
+    };
+    $.fn.dropdown.Constructor = dropdownConstructor;
